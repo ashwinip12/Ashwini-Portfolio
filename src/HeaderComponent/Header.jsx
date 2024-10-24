9a55c29220fab2ac6c24f96eb9ee20a551329313
ashwinip12 <ashwinibms2020@gmail.com>
2024-10-24 14:34:28 +0530

    added header component

---
+++ src/HeaderComponent/Header.jsx
@@ -0,0 +1,27 @@
+
+
+
+import React from "react";
+import './_Header.scss';
+
+const Header = () => {
+  return (
+    <header className="header-container">
+      <div className="header-left">
+        <div className="logo">
+          <h1>Ashwini</h1>
+        </div>
+      </div>
+      <div className="header-right">
+        <div className="nav-links">
+          <a href="">About me</a>
+          <a href="">Contact</a>
+          <a href="">Skills</a>
+          <a href="">Certificate</a>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;

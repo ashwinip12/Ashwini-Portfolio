4bccc10c839452f1e889fc931a14d07ea23f4988
ashwinip12 <ashwinibms2020@gmail.com>
2024-10-24 15:50:38 +0530

    Added skills component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Header from './HeaderComponent/Header';
 import {Main} from './MainComponent/Main';
+import { Skill } from './SkillComponent/Skill';
 
 function App() {
   return (
     <div className="App">
       <Header/>
      <Main/>
+     <Skill/>
     </div>
   );
 }

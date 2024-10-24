ec93a7637c043ec49651191dbde3e04cd04db28d
ashwinip12 <ashwinibms2020@gmail.com>
2024-10-24 15:13:50 +0530

    added main component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './HeaderComponent/Header';
+import {Main} from './MainComponent/Main';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+     <Main/>
     </div>
   );
 }

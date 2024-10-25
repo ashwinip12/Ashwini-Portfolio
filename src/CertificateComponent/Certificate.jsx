343ac7b1062e82af19bfdc674246ec5237371dd4
ashwinip12 <ashwinibms2020@gmail.com>
2024-10-25 10:54:17 +0530

    made changes

---
+++ src/CertificateComponent/Certificate.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './_Certificate.scss';
+
+export const Certificate=()=>
+{
+return(
+    <div className="Certificate-container">
+        <h1>Certificate..</h1>
+    </div>
+    )
+
+}
--- src/ProjectComponent/Project.jsx
+++ src/ProjectComponent/Project.jsx
@@ -1,42 +1,12 @@
-import React from 'react';
-import './_Project.scss'; 
+import React from "react";
+import './_Project.scss'
 
-const Project = ({ title, author, date, tags, content, imageUrl }) => {
-  return (
-    <div>
-      <div className='Project-container'>
-       <h2>Project</h2>
-      </div>
-      
-      
-      <div className="blogCard">
-        <div className="meta">
-          <div
-            className="photo"
-            style={{ backgroundImage: `url(${imageUrl})` }}
-          ></div>
-          <ul className="details">
-            <li className="author">{author}</li>
-            <li className="date">{date}</li>
-            <li className="tags">
-              <ul>
-                {tags.map((tag, index) => (
-                  <li key={index}>{tag}</li>
-                ))}
-              </ul>
-            </li>
-          </ul>
-        </div>
-        <div className="description">
-          <h1>{title}</h1>
-          <p>{content}</p>
-          <p className="readMore">
-            <a href="#">Read More</a>
-          </p>
-        </div>
-      </div>
+export const Project=()=>
+{
+return(
+    <div className="Project-container">
+        <h1>Projects..</h1>
     </div>
-  );
-};
+    )
 
-export default Project;
+}

0c6611d7eb7fae63e186a534da5de26612417560
ashwinip12 <ashwinibms2020@gmail.com>
2024-10-26 17:04:10 +0530

    added illustrator in header

--- src/ProjectComponent/Project.jsx
+++ src/ProjectComponent/Project.jsx
@@ -16,11 +16,11 @@ import './_Project.scss';
 
 export const Project = () => {
   return (
-    <section className="car">
+    <section className="card-section">
     <h2 className="card__title1">Projects</h2>
     <div className='cards'>
       
-      <div className="cards">
+    
       <article className="card card--1">
       
         <div className="card__img card__img--1"></div>
@@ -52,19 +52,10 @@ export const Project = () => {
           <span className="card__by">by <a href="#" className="card__author">John Doe</a></span>
         </div>
       </article>
-       <article className="card card--2">
-
-        <div className="card__img card__img--2"></div>
-
-        <div className="card__info">
-          <span className="card__category">Travel</span>
-          <h3 className="card__title">Discover the sea</h3>
-          <span className="card__by">by <a href="#" className="card__author">John Doe</a></span>
-        </div>
-      </article>
+     
       
       </div>
-      </div>
+      
     </section>
   );
 };
